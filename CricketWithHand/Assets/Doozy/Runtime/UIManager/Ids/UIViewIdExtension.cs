30f4b06fc3c66b5db23f06025d909eebb27619e8
Yash Sadhukhan <yash.s@virtualizetechnologies.com>
2024-09-28 14:03:27 +0530

    Updated Registration View

--- CricketWithHand/Assets/Doozy/Runtime/UIManager/Ids/UIViewIdExtension.cs
+++ CricketWithHand/Assets/Doozy/Runtime/UIManager/Ids/UIViewIdExtension.cs
@@ -21,6 +21,10 @@ namespace Doozy.Runtime.UIManager.Containers
         public static IEnumerable<UIView> GetViews(UIViewId.Gameplay id) => GetViews(nameof(UIViewId.Gameplay), id.ToString());
         public static void Show(UIViewId.Gameplay id, bool instant = false) => Show(nameof(UIViewId.Gameplay), id.ToString(), instant);
         public static void Hide(UIViewId.Gameplay id, bool instant = false) => Hide(nameof(UIViewId.Gameplay), id.ToString(), instant);
+
+        public static IEnumerable<UIView> GetViews(UIViewId.MainMenu id) => GetViews(nameof(UIViewId.MainMenu), id.ToString());
+        public static void Show(UIViewId.MainMenu id, bool instant = false) => Show(nameof(UIViewId.MainMenu), id.ToString(), instant);
+        public static void Hide(UIViewId.MainMenu id, bool instant = false) => Hide(nameof(UIViewId.MainMenu), id.ToString(), instant);
     }
 }
 
@@ -47,6 +51,12 @@ namespace Doozy.Runtime.UIManager
             HalfTimeView,
             TossResultView,
             TossView
+        }
+
+        public enum MainMenu
+        {
+            Login,
+            Registration
         }    
     }
 }
